329a501b0e1f1663f088082c3c53254c04b6db94
Faiza Shirazi <ed19fs2@leeds.ac.uk>
2025-02-02 19:37:22 +0000

    Really simply login system that only asks for a password.

---
+++ prototype/LoginSystem/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Contact from './contact'
+
+function MyComponent() {
+  return <h1>Hello world</h1>;
+}
+
+
+function App() {
+  return <Contact />;
+}
+
+export {App as App, MyComponent as MyComponent};
