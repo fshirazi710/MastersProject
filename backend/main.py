1af7e86750cd8c987e1cb77bd74b9fff0e10b2a4
Febin Shaji <shaji.febin@yahoo.com>
2025-02-10 16:40:07 +0000

    created api.ini file to act as a config file

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from app.routers.vote_router import router as vote_router
 from starlette.middleware.cors import CORSMiddleware
+from app.constants import CORS_ALLOWED_ORIGINS
 
 app = FastAPI()
 
 # Allow requests from the frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[CORS_ALLOWED_ORIGINS],
     allow_credentials=True,
     allow_methods=["*"], # Allow all HTTP methods (GET, POST, etc...)
     allow_headers=["*"], # Allow all headers
