dc95b07b20efaceb5a1ae5bf35fe7f7eca512b5d
Faiza Shirazi <ed19fs2@leeds.ac.uk>
2025-04-07 20:16:01 +0100

    Script for making firefox profiles complete

--- frontend/tests/get_all_profiles.py
+++ frontend/tests/get_all_profiles.py
@@ -1,11 +1,29 @@
 
 from pathlib import Path
 import subprocess
+import time
+import shutil
+import os
+from pathlib import Path
 
 for x in range(0, 3):
+    # Get profile path
     profile_path = Path(f"test_profiles/testprofile_{x}").resolve()
+    
+    if Path(profile_path).is_dir():
+        shutil.rmtree(profile_path)
 
     profile_path.mkdir(parents=True, exist_ok=True)
-    instr = f'firefox -profile "{profile_path}"'
+    
+    # Setup instruction for making profile -headless
+    instr = f'firefox -profile -headless -no-remote "{profile_path}"'
     print(instr)
-    subprocess.run(instr, shell=True, check=True)
+
+    # Run instruction
+    proc = subprocess.Popen(instr, shell=True)
+
+    # Let the instruction run
+    time.sleep(1)
+
+    # Close firefox window that shows up after making profile
+    proc.terminate()
